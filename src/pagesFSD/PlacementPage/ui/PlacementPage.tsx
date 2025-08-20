043ff55e864bd5b804f6bab3e6d7795b3a0fb0d4
ZhenyaNeh <zhenya.nehaychik@gmail.com>
2025-08-20 14:05:48 +0300

    feat: add placement page

---
+++ src/pagesFSD/PlacementPage/ui/PlacementPage.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import React, { FC } from 'react';
+import { Board, BoardComponentTypes } from '@/src/features/Board';
+import { PlacementPanel } from '@/src/features/PlacementPanel/ui/PlacementPanel';
+import { ShipDock } from '@/src/features/ShipDock/ui/ShipDock';
+import { usePlacement } from '@/src/shared/hooks/usePlacement/usePlacement';
+import { DndContext, MouseSensor, TouchSensor, useSensor } from '@dnd-kit/core';
+import { restrictToWindowEdges } from '@dnd-kit/modifiers';
+
+const PlacementPage: FC = () => {
+  const {
+    handleDragEnd,
+    handleDragOver,
+    handleDragCancel,
+    handleDragAbort,
+    customCollisionDetection,
+  } = usePlacement();
+
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10,
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 10,
+      tolerance: 5,
+    },
+  });
+
+  return (
+    <div>
+      <DndContext
+        onDragOver={handleDragOver}
+        onDragEnd={handleDragEnd}
+        onDragCancel={handleDragCancel}
+        onDragAbort={handleDragAbort}
+        sensors={[mouseSensor, touchSensor]}
+        modifiers={[restrictToWindowEdges]}
+        collisionDetection={customCollisionDetection}
+      >
+        <div
+          id='main-container'
+          className='w-full flex justify-center items-center flex-wrap'
+        >
+          <h1 className='w-full text-2x text-center font-bold text-3xl mb-10'>
+            Arrange the ships
+          </h1>
+          <div className='flex justify-between items-top w-full flex-wrap'>
+            <ShipDock />
+            <Board type={BoardComponentTypes.PLACEMENT} />
+            <PlacementPanel />
+          </div>
+        </div>
+      </DndContext>
+    </div>
+  );
+};
+
+export default PlacementPage;
